7310ddfcdc1300842253d53097741f05580cf042
Chloe Pappas <chloeoliviapappas@gmail.com>
2021-04-27 21:06:14 -0400

    Building add and remove methods

--- src/health_records_system.py
+++ src/health_records_system.py
@@ -1,12 +1,38 @@
 class HealthRecordsSystem:
-    patients = []
-    
-    #def __init__(self):
+
+    def __init__(self):
+        self.__patients = {}
         
     def add_patient(self, patient):
-        self.patients[patient] = None
+        if patient.ID in self.__patients:
+            overwrite = input(f"Patient #{patient.ID} already exists. Overwrite? Y/N")
+            if overwrite == "Y":
+                self.__patients[patient.ID] = patient
+                print(f"Patient #{patient.ID} successfully added to the system.")
+            else:
+                print(f"Failure: Patient #{patient.ID} was not added to the system.")
+        else:
+            self.__patients[patient.ID] = patient
+            print(f"Patient #{patient.ID} successfully added to the system.")           
+        
+    def remove_patient(self, ID):
+        try:
+            del self.__patients[ID]
+        except KeyError:
+            print(f"Patient #{patient.ID} could not be removed because it does not exist in the system.")
 
+    def print_patients(self):
+        for (name, info) in self.__patients.items():
+            print(f"Patient: {name}, Information: {info}")
 
 class Patient:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, ID, name, age):
+        self.__ID = ID
+        self.__name = name
+        self.__age = age
+
+    @property
+    def ID(self):
+        return self.__ID
+
+        
--- src/main.py
+++ src/main.py
@@ -1,7 +1,44 @@
-from health_records_system import HealthRecordsSystem
+from health_records_system import HealthRecordsSystem, Patient
 
 def main():
-    system = HealthRecordsSystem()   
+    system = HealthRecordsSystem()
+    
+    while True:
+        option = input("""
+        ----- Electronic Health Records System -----
+        Enter a number to select an option:
+        1. View patient records
+        2. Add a new patient
+        3. Delete a patient's file
+        4. Cancel
+        """)
+    
+        try:
+            option = int(option)
+        except ValueError:
+            print("Please enter a number between 1 and 4.")
+            continue
+
+        if option == 1:
+            system.print_patients()
+            
+        elif option == 2:
+            ID = input("Please input the patient's ID number: ")
+            name = input("Please input the patient's name: ")
+            age = input("Please input the patient's age: ")
+            patient = Patient(ID, name, age)
+            system.add_patient(patient)
+        
+        elif option == 3:
+            remove_ID = input("Please input the ID number of the patient that you want to remove:")
+            system.remove_patient(remove_ID)
+
+        elif option == 4:
+            break
+
+        else:
+            print("Please enter a number between 1 and 4.")
+
     
 if __name__ == "__main__":
     main()
