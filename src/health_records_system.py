3d87c872f06be5739fd18865d356d1d99a413964
Chloe Pappas <chloeoliviapappas@gmail.com>
2021-05-02 13:44:45 -0400

    Adding medication functionality

--- src/health_records_system.py
+++ src/health_records_system.py
@@ -2,7 +2,10 @@ class HealthRecordsSystem:
 
     def __init__(self):
         self.__patients = {}
-        
+
+    def get_patient(self, ID):
+        return self.__patients[ID]
+
     def add_patient(self, patient):
         if patient.ID in self.__patients:
             overwrite = input(f"Patient #{patient.ID} already exists. Overwrite? Y/N")
@@ -18,9 +21,11 @@ class HealthRecordsSystem:
     def remove_patient(self, ID):
         try:
             del self.__patients[ID]
+            print(f"Patient #{ID} successfully removed from the system.")
         except KeyError:
-            print(f"Patient #{patient.ID} could not be removed because it does not exist in the system.")
-
+            print(f"Patient #{ID} does not exist in the system.")
+            return
+        
     def print_patients(self):
         for (name, info) in self.__patients.items():
             print(f"Patient: {name}, Information: {info}")
@@ -30,9 +35,27 @@ class Patient:
         self.__ID = ID
         self.__name = name
         self.__age = age
+        self.__medication = {}        
 
     @property
     def ID(self):
         return self.__ID
+    
+    @property
+    def medication(self):
+        return self.__medication
+    
+    def add_medication(self, medication, dosage, frequency):
+        if medication in self.__medication:
+            overwrite = input(f"Patient #{patient.ID} is already taking this medication. Overwrite dosage and frequency? Y/N")
+            if overwrite == "Y":
+                self.__medication[medication] = (dosage, frequency)
+                print(f"Medication successfully updated in patient #{ID}'s record.")
+            else:
+                print(f"Failure: Medication was not updated in patient #{ID}'s record.")
+        else:
+                self.__medication[medication] = (dosage, frequency)
+                print(f"Medication successfully added to patient #{ID}'s record.")   
+     
 
         
--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ def main():
         option = input("""
         ----- Electronic Health Records System -----
         Enter a number to select an option:
-        1. View patient records
+        1. View and edit patient records
         2. Add a new patient
         3. Delete a patient's file
         4. Cancel
@@ -18,27 +18,62 @@ def main():
         except ValueError:
             print("Please enter a number between 1 and 4.")
             continue
+        
+        if option != 4:
+            ID = input("Please input the patient's ID number: ")
 
         if option == 1:
-            system.print_patients()
-            
+            try:
+                patient = system.get_patient(ID)
+                view_edit_records(patient)
+            except KeyError:
+                print(f"Patient #{ID} does not exist in the system.")
+                continue
+
         elif option == 2:
-            ID = input("Please input the patient's ID number: ")
             name = input("Please input the patient's name: ")
             age = input("Please input the patient's age: ")
             patient = Patient(ID, name, age)
             system.add_patient(patient)
         
         elif option == 3:
-            remove_ID = input("Please input the ID number of the patient that you want to remove:")
-            system.remove_patient(remove_ID)
+            system.remove_patient(ID)
 
         elif option == 4:
-            break
+            print("Thank you for using the Electronic Health Records System!")
+            return
 
         else:
             print("Please enter a number between 1 and 4.")
 
     
+    
+def view_edit_records(patient):
+    while True:
+        option = input("""
+        ----- View and Edit Patient Records -----
+            Enter a number to select an option:
+            1. View medication
+            2. Add medication
+            3. Remove medication
+            4. Cancel
+            """)
+
+        try:
+            option = int(option)
+        except ValueError:
+            print("Please enter a number between 1 and 4.")
+            continue
+
+        if option == 1:
+            for medication, (dosage, frequency) in patient.medication.items():
+                print(f"Medication: #{medication}, Dosage: #{dosage}, Frequency: #{frequency}\n")
+
+        elif option == 2:
+            medication = input("Please input the name of the medication: ")
+            dosage = input("Please input the dosage: ")
+            frequency = input("Please input the frequency of this dosage: ")
+            patient.add_medication(medication, dosage, frequency)
+
 if __name__ == "__main__":
     main()
