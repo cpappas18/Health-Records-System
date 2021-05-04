5e457c9633f78f16675c9c457705afb49f2372b1
Chloe Pappas <chloeoliviapappas@gmail.com>
2021-05-04 16:03:17 -0400

    Added functionality for storing test results

--- src/health_records_system.py
+++ src/health_records_system.py
@@ -1,59 +1,93 @@
-class HealthRecordsSystem:
+class HealthRecordsSystem(object):
 
     def __init__(self):
         self.__patients = {}
 
-    def get_patient(self, ID):
-        return self.__patients[ID]
+    def get_patient(self, id):
+        return self.__patients[id]
 
     def add_patient(self, patient):
-        if patient.ID in self.__patients:
-            overwrite = input(f"Patient #{patient.ID} already exists. Overwrite? Y/N")
+        if patient.id in self.__patients:
+            overwrite = input(f"Patient #{patient.id} already exists. Overwrite? Y/N")
             if overwrite == "Y":
-                self.__patients[patient.ID] = patient
-                print(f"Patient #{patient.ID} successfully added to the system.")
+                self.__patients[patient.id] = patient
+                print(f"Patient #{patient.id} successfully added to the system.")
             else:
-                print(f"Failure: Patient #{patient.ID} was not added to the system.")
+                print(f"Failure: Patient #{patient.id} was not added to the system.")
+                return
         else:
-            self.__patients[patient.ID] = patient
-            print(f"Patient #{patient.ID} successfully added to the system.")           
+            self.__patients[patient.id] = patient
+            print(f"Patient #{patient.id} successfully added to the system.")
         
-    def remove_patient(self, ID):
+    def remove_patient(self, id):
         try:
-            del self.__patients[ID]
-            print(f"Patient #{ID} successfully removed from the system.")
+            del self.__patients[id]
+            print(f"Patient #{id} successfully removed from the system.")
         except KeyError:
-            print(f"Patient #{ID} does not exist in the system.")
+            print(f"Patient #{id} does not exist in the system.")
             return
         
     def print_patients(self):
         for (name, info) in self.__patients.items():
             print(f"Patient: {name}, Information: {info}")
 
-class Patient:
-    def __init__(self, ID, name, age):
-        self.__ID = ID
+
+class Patient(object):
+
+    def __init__(self, id, name, age, phone_number):
+        self.__id = id
         self.__name = name
         self.__age = age
-        self.__medication = {}        
+        self.__phone_number = phone_number
+        self.__medication = {}
+        self.__test_results = {}
 
     @property
-    def ID(self):
-        return self.__ID
-    
+    def id(self):
+        return self.__id
+
+    @property
+    def phone_number(self):
+        return self.__phone_number
+
     @property
     def medication(self):
         return self.__medication
+
+    @property
+    def test_results(self):
+        return self.__test_results
     
     def add_medication(self, medication, dosage, frequency):
         if medication in self.__medication:
-            overwrite = input(f"Patient #{self.__ID} is already taking this medication. Overwrite dosage and frequency? Y/N")
+            overwrite = input(f"Patient #{self.__id} is already taking this medication. Overwrite dosage and frequency? Y/N")
             if overwrite == "Y":
                 self.__medication[medication] = (dosage, frequency)
-                print(f"Medication successfully updated in patient #{self.__ID}'s record.")
+                print(f"Medication successfully updated in patient #{self.__id}'s record.")
             else:
-                print(f"Failure: Medication was not updated in patient #{self.__ID}'s record.")
+                print(f"Failure: Medication was not updated in patient #{self.__id}'s record.")
+                return
         else:
             self.__medication[medication] = (dosage, frequency)
-            print(f"Medication successfully added to patient #{self.__ID}'s record.")
+            print(f"Medication successfully added to patient #{self.__id}'s record.")
 
+    def remove_medication(self, name):
+        try:
+            del self.__medication[name]
+            print(f"Medication successfully removed from patient #{self.__id}'s record.")
+        except KeyError:
+            print(f"Failure: Medication does not exist in patient #{self.__id}'s record.")
+            return
+
+    def add_test_results(self, name, date, result):
+        if (name, date) in self.__test_results:
+            overwrite = input(f"A result for this test on {date} has already been recorded. Overwrite test result? Y/N")
+            if overwrite == "Y":
+                self.__test_results[(name, date)] = result
+                print(f"Test result successfully updated in patient #{self.__id}'s record.")
+            else:
+                print(f"Failure: Test result was not updated in patient #{self.__id}'s record.")
+                return
+        else:
+            self.__test_results[(name, date)] = result
+            print(f"Test result successfully added to patient #{self.__id}'s record.")
--- src/main.py
+++ src/main.py
@@ -21,24 +21,25 @@ def main():
             continue
         
         if option != 4:
-            ID = input("Please input the patient's ID number: ")
+            id = input("Please input the patient's ID number: ")
 
         if option == 1:
             try:
-                patient = system.get_patient(ID)
+                patient = system.get_patient(id)
                 view_edit_records(patient)
             except KeyError:
-                print(f"Patient #{ID} does not exist in the system.")
+                print(f"Patient #{id} does not exist in the system.")
                 continue
 
         elif option == 2:
             name = input("Please input the patient's name: ")
             age = input("Please input the patient's age: ")
-            patient = Patient(ID, name, age)
+            phone_number = input("Please input the patient's telephone number: ")
+            patient = Patient(id, name, age, phone_number)
             system.add_patient(patient)
         
         elif option == 3:
-            system.remove_patient(ID)
+            system.remove_patient(id)
 
         elif option == 4:
             print("Thank you for using the Electronic Health Records System!")
@@ -53,27 +54,53 @@ def view_edit_records(patient):
         option = input("""
         ----- View and Edit Patient Records -----
             Enter a number to select an option:
-            1. View medication
-            2. Add medication
-            3. Remove medication
-            4. Cancel
+            1. Get patient contact information
+            2. View medication
+            3. Add medication
+            4. Remove medication
+            5. View test results
+            6. Add test results
+            7. Cancel
             """)
 
         try:
             option = int(option)
         except ValueError:
-            print("Please enter a number between 1 and 4.")
+            print("Please enter a number between 1 and 7.\n")
             continue
 
         if option == 1:
+            print(f"Phone number: {patient.phone_number}")
+
+        elif option == 2:
             for medication, (dosage, frequency) in patient.medication.items():
                 print(f"Medication: {medication}, Dosage: {dosage}, Frequency: {frequency}\n")
 
-        elif option == 2:
-            medication = input("Please input the name of the medication: ")
+        elif option == 3:
+            medication_name = input("Please input the name of the medication: ")
             dosage = input("Please input the dosage: ")
             frequency = input("Please input the frequency of this dosage: ")
-            patient.add_medication(medication, dosage, frequency)
+            patient.add_medication(medication_name, dosage, frequency)
+
+        elif option == 4:
+            medication_name = input("Please input the name of the medication: ")
+            patient.remove_medication(medication_name)
+
+        elif option == 5:
+            for (name, date), result in patient.test_results.items():
+                print(f"Test: {name}, Date: {date}, Result: {result}\n")
+
+        elif option == 6:
+            test_name = input("Please input the name of the test: ")
+            date = input("Please input the date that the test was performed (DD/MM/YYYY): ")
+            result = input("Please input the test result: ")
+            patient.add_test_results(test_name, date, result)
+
+        elif option == 7:
+            return
+
+        else:
+            print("Please enter a number between 1 and 7.")
 
 
 if __name__ == "__main__":
