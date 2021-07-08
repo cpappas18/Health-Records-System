1abf9c0e778481588c1c988a8ec6d77daf228558
Chloe <49031258+cpappas18@users.noreply.github.com>
2021-07-08 16:52:13 -0400

    Adding tests

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from unittest.mock import Mock
 from src.command import *
 
 
@@ -8,12 +9,23 @@ class TestInvoker(TestCase):
         self.invoker = Invoker()
 
     def test_register(self):
-        command = ICommand()
-        self.invoker.register(command)
-        self.assertEqual(self.invoker._commands, [command])
+        command_stub = Mock(ICommand)
+        self.invoker.register(command_stub)
+        self.assertEqual(self.invoker._commands, [command_stub])
 
-    def test_execute(self):
+    def test_execute_for_none_undone(self):
+        command_stub = Mock(ICommand)
+        self.invoker.register(command_stub)
+        self.invoker.execute(command_stub, "arg")
+        self.assertEqual(self.invoker._history, [(command_stub, ("arg",))])
+
+    def test_execute_for_some_undone(self):
         self.fail()
+
+    def test_execute_for_command_not_registered(self):
+        command_stub = Mock(ICommand)
+        self.invoker.execute(command_stub, "arg")
+        self.assertEqual(self.invoker._history, [])
 
     def test_undo(self):
         self.fail()
